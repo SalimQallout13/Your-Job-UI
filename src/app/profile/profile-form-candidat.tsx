9d09c1dac87ffbf12c77e8ada2a9098478589c52
Salim <s.qallout@hexacoffre.com>
2024-12-09 17:00:04 +0100

    finish layout candidat profile

--- src/app/profile/profile-form-candidat.tsx
+++ src/app/profile/profile-form-candidat.tsx
@@ -1,34 +1,35 @@
 import React from "react"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Message } from "@/components/ui/alert.tsx"
+import {Button} from "@/components/ui/button"
+import {Input} from "@/components/ui/input"
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form"
+import {Message} from "@/components/ui/alert.tsx"
 import Dropzone from "@/components/ui/dropzone.tsx"
-import { useProfileFormCandidat } from "@/lib/hooks/profile/use-profile-form-candidat.ts"
+import {useProfileFormCandidat} from "@/lib/hooks/profile/use-profile-form-candidat.ts"
+import Title from "@/components/ui/title.tsx";
 
 const ProfileFormCandidat: React.FC = () => {
 
-	const {
-		profileFormSecondSchema,
-		isSubmitting,
-		errorMessage,
-		submitProfileFormSecond,
-		isLoading
-	} = useProfileFormCandidat()
+    const {
+        profileFormSecondSchema,
+        isSubmitting,
+        errorMessage,
+        submitProfileFormSecond,
+        isLoading
+    } = useProfileFormCandidat()
 
 
-	if (isLoading) {
-		// Affichez un indicateur de chargement tant que les valeurs par défaut ne sont pas prêtes
-		return <div>Chargement...</div>
-	}
+    if (isLoading) {
+        // Affichez un indicateur de chargement tant que les valeurs par défaut ne sont pas prêtes
+        return <div>Chargement...</div>
+    }
 
-	return (
-		<>
-			<Form {...profileFormSecondSchema}>
-				{errorMessage && <Message message={errorMessage} type={"alert"} />}
-				<form>
-					<h2 className="mb-6 mt-8 text-2xl font-bold">Informations professionnelles</h2>
-					{/*<FormField
+    return (
+        <>
+            <Form {...profileFormSecondSchema}>
+                {errorMessage && <Message message={errorMessage} type={"alert"}/>}
+                <form>
+                    <Title className="mb-7">Informations professionnelles</Title>
+                    {/*<FormField
 						control={profileFormSecondSchema.control}
 						name="biographie"
 						render={({ field }) => (
@@ -42,114 +43,117 @@ const ProfileFormCandidat: React.FC = () => {
 							</FormItem>
 						)}
 					/>*/}
+                    <div className="p-6">
 
-					<div className="mb-4 grid grid-cols-1 gap-6 md:grid-cols-2">
-						<FormField
-							control={profileFormSecondSchema.control}
-							name="currentPoste"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Poste actuel (facultatif)</FormLabel>
-									<FormControl>
-										<Input placeholder="Graphic Designer" className="h-12" {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={profileFormSecondSchema.control}
-							name="ville"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Ville</FormLabel>
-									<FormControl>
-										<Input placeholder="Marseille" className="h-12" {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
+                        <div className="mb-7 grid grid-cols-1 gap-6 md:grid-cols-2">
+                            <FormField
+                                control={profileFormSecondSchema.control}
+                                name="currentPoste"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Poste actuel (facultatif)</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Graphic Designer" className="h-12" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={profileFormSecondSchema.control}
+                                name="ville"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Ville</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Marseille" className="h-12" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
 
-						<FormField
-							control={profileFormSecondSchema.control}
-							name="codePostal"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Code postal</FormLabel>
-									<FormControl>
-										<Input placeholder="13001" className="h-12" {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={profileFormSecondSchema.control}
-							name="adresse"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Adresse</FormLabel>
-									<FormControl>
-										<Input placeholder="12 rue de Jouy" className="h-12" {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={profileFormSecondSchema.control}
-							name="cv"
-							render={({ field }) => (
-								<FormItem className="space-y-4">
-									<FormLabel>CV</FormLabel>
-									<FormControl>
-										<Dropzone
-											accept="application/pdf"
-											maxSize={10 * 1024 * 1024}
-											defaultFile={field.value}
-											onFileChange={(file) => {
-												field.onChange(file)
-											}}
-										/>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
+                            <FormField
+                                control={profileFormSecondSchema.control}
+                                name="codePostal"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Code postal</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="13001" className="h-12" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={profileFormSecondSchema.control}
+                                name="adresse"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Adresse</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="12 rue de Jouy" className="h-12" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={profileFormSecondSchema.control}
+                                name="cv"
+                                render={({field}) => (
+                                    <FormItem className="space-y-4">
+                                        <FormLabel>CV</FormLabel>
+                                        <FormControl>
+                                            <Dropzone
+                                                accept="application/pdf"
+                                                maxSize={10 * 1024 * 1024}
+                                                defaultFile={field.value}
+                                                onFileChange={(file) => {
+                                                    field.onChange(file)
+                                                }}
+                                            />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
 
-						<FormField
-							control={profileFormSecondSchema.control}
-							name="lettreMotivation"
-							render={({ field }) => (
-								<FormItem className="space-y-4">
-									<FormLabel>Lettre de motivation</FormLabel>
-									<FormControl>
-										<Dropzone
-											accept="application/pdf"
-											maxSize={10 * 1024 * 1024}
-											defaultFile={field.value}
-											onFileChange={(file) => {
-												field.onChange(file)
-											}}
-										/>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-					</div>
+                            <FormField
+                                control={profileFormSecondSchema.control}
+                                name="lettreMotivation"
+                                render={({field}) => (
+                                    <FormItem className="space-y-4">
+                                        <FormLabel>Lettre de motivation</FormLabel>
+                                        <FormControl>
+                                            <Dropzone
+                                                accept="application/pdf"
+                                                maxSize={10 * 1024 * 1024}
+                                                defaultFile={field.value}
+                                                onFileChange={(file) => {
+                                                    field.onChange(file)
+                                                }}
+                                            />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
 
-					<FormControl>
-						<Button variant="gradient" onClick={profileFormSecondSchema.handleSubmit(submitProfileFormSecond)}
-										disabled={isSubmitting}>
-							{isSubmitting ? "Enregistrement en cours..." : "Enregistrer"}
-						</Button>
-					</FormControl>
-				</form>
-			</Form>
-		</>
-	)
+                        <FormControl>
+                            <Button variant="gradient"
+                                    onClick={profileFormSecondSchema.handleSubmit(submitProfileFormSecond)}
+                                    disabled={isSubmitting}>
+                                {isSubmitting ? "Enregistrement en cours..." : "Enregistrer"}
+                            </Button>
+                        </FormControl>
+                    </div>
+                </form>
+            </Form>
+        </>
+    )
 }
 
 export default ProfileFormCandidat
--- src/lib/hooks/profile/use-profile-form-candidat.ts
+++ src/lib/hooks/profile/use-profile-form-candidat.ts
@@ -1,70 +1,70 @@
-import { useNavigationContext } from "@/lib/context/navigation-context.tsx"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { updateProfileCandidat } from "@/api/profile-api.ts"
-import { showErrorToast, showToast } from "@/lib/hooks/use-toast.tsx"
-import { useSessionContext } from "@/lib/context/session-context.tsx"
-import { useEffect, useState } from "react"
-import { baseProfileCandidatSchema, BaseProfileCandidatSchema } from "@/lib/schemas-validation-form/userValidation.ts"
+import {useNavigationContext} from "@/lib/context/navigation-context.tsx"
+import {useForm} from "react-hook-form"
+import {zodResolver} from "@hookform/resolvers/zod"
+import {updateProfileCandidat} from "@/api/profile-api.ts"
+import {showErrorToast, showToast} from "@/lib/hooks/use-toast.tsx"
+import {useSessionContext} from "@/lib/context/session-context.tsx"
+import {useEffect, useState} from "react"
+import {baseProfileCandidatSchema, BaseProfileCandidatSchema} from "@/lib/schemas-validation-form/userValidation.ts"
 
 export const useProfileFormCandidat = () => {
 
-	const { isSubmitting, errorMessage, setIsSubmitting } = useNavigationContext()
-	const { userData, updateUserData, candidatData, convertToFile } = useSessionContext()
+    const {isSubmitting, errorMessage, setIsSubmitting} = useNavigationContext()
+    const {userData, updateUserData, candidatData, convertToFile} = useSessionContext()
 
-	const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true)
 
 
-	const profileFormSecondSchema = useForm<BaseProfileCandidatSchema>({
-		resolver: zodResolver(baseProfileCandidatSchema),
-		defaultValues: {}
-	})
+    const profileFormSecondSchema = useForm<BaseProfileCandidatSchema>({
+        resolver: zodResolver(baseProfileCandidatSchema),
+        defaultValues: {}
+    })
 
-	useEffect(() => {
-		const loadDefaults = async () => {
-			if (userData) {
-				const defaultValues = {
-					currentPoste: candidatData.currentPoste,
-					ville: userData?.ville || "",
-					codePostal: userData?.codePostal || "",
-					adresse: userData?.adresse || "",
-					cv: await convertToFile(candidatData.cv),
-					lettreMotivation: await convertToFile(candidatData.lettreMotivation)
-				}
-				console.log("defaultValues", defaultValues)
-				profileFormSecondSchema.reset(defaultValues)
-			}
-			setIsLoading(false) // Indique que le chargement est terminé
-		}
-		loadDefaults()
-	}, [userData, convertToFile, profileFormSecondSchema, candidatData])
+    useEffect(() => {
+        const loadDefaults = async () => {
+            if (userData) {
+                const defaultValues = {
+                    currentPoste: candidatData.currentPoste,
+                    ville: userData?.ville || "",
+                    codePostal: userData?.codePostal || "",
+                    adresse: userData?.adresse || "",
+                    cv: await convertToFile(candidatData.cv),
+                    lettreMotivation: await convertToFile(candidatData.lettreMotivation)
+                }
+                console.log("defaultValues", defaultValues)
+                profileFormSecondSchema.reset(defaultValues)
+            }
+            setIsLoading(false) // Indique que le chargement est terminé
+        }
+        loadDefaults()
+    }, [userData, convertToFile, profileFormSecondSchema, candidatData])
 
-	const submitProfileFormSecond = async (data: BaseProfileCandidatSchema) => {
-		try {
-			setIsSubmitting(true)
-			if (userData) {
-				const response = await updateProfileCandidat(userData._id, data)
-				if (response.status === "success") {
-					showToast("Succès", "Profil mis à jour", false)
-					updateUserData(response.data)
-				} else {
-					showErrorToast(response.error)
-				}
-			} else {
-				throw new Error("Impossible de mettre à jour le profil : utilisateur introuvable")
-			}
-		} catch (error) {
-			throw new Error(error instanceof Error ? error.message : "Une erreur est survenue lors de l'inscription")
-		} finally {
-			setIsSubmitting(false)
-		}
-	}
+    const submitProfileFormSecond = async (data: BaseProfileCandidatSchema) => {
+        try {
+            setIsSubmitting(true)
+            if (userData) {
+                const response = await updateProfileCandidat(userData._id, data)
+                if (response.status === "success") {
+                    showToast("Succès", "Profil mis à jour", false)
+                    updateUserData(response.data)
+                } else {
+                    showErrorToast(response.error)
+                }
+            } else {
+                throw new Error("Impossible de mettre à jour le profil : utilisateur introuvable")
+            }
+        } catch (error) {
+            throw new Error(error instanceof Error ? error.message : "Une erreur est survenue lors de l'inscription")
+        } finally {
+            setIsSubmitting(false)
+        }
+    }
 
-	return {
-		profileFormSecondSchema,
-		isSubmitting,
-		errorMessage,
-		submitProfileFormSecond,
-		isLoading
-	}
+    return {
+        profileFormSecondSchema,
+        isSubmitting,
+        errorMessage,
+        submitProfileFormSecond,
+        isLoading
+    }
 }
